26c751ce8d94791d50b66cb68734b42ec6a5f7f3
Buriiboo <Buraian@live.se>
2023-10-17 12:31:42 +0200

    test igen

---
+++ Game.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Questions;
+{
+
+}
+
+namespace Game
+{
+    public class QuizApplication
+{
+    private List<Freetext> questions;
+    private List<Freetext> askedQuestions;
+
+    public QuizApplication()
+    {
+        questions = new List<Freetext>();
+        askedQuestions = new List<Freetext>();
+    }
+
+    public Freetext GetRandomQuestion()
+    {
+        if (questions.Count == 0)
+            return null;
+
+        Random random = new Random();
+        int index = random.Next(questions.Count);
+        Freetext randomQuestion = questions[index];
+
+        // Remove the question from the available questions
+        questions.RemoveAt(index);
+
+        // Add the question to the asked questions
+        askedQuestions.Add(randomQuestion);
+
+        return randomQuestion;
+    }
+}
+
+}
